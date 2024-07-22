20b21fbf1f69537297c1f0bdc695f03da983b646
kinoi1 <agilsupriyanto14@gmail.com>
2024-07-22 09:13:35 +0700

    fixing issue verify token

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -9,13 +9,13 @@ export const verifyToken = (req, res, next) => {
         res.email = decode.email;
         next();
     })
-    if (authHeader) {
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-            if (err) return res.sendStatus(403);
-            req.user = user;
-            next();
-        });
-    } else {
-        res.sendStatus(401);
-    }
+    // if (authHeader) {
+    //     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    //         if (err) return res.sendStatus(403);
+    //         req.user = user;
+    //         next();
+    //     });
+    // } else {
+    //     res.sendStatus(401);
+    // }
 }
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,7 +4,7 @@ import { verifyToken } from "../middleware/verifyToken.js";
 import { refreshToken } from "../controllers/RefreshToken.js";
 const router = express.Router();
 
-router.get('/users', getUsers);
+router.get('/users',verifyToken, getUsers);
 router.post('/users', Register);
 router.post('/login', Login);
 router.post('/token', refreshToken);
